137c45d1bc4783507beda602dd677e307eda952a
Bart van der Schans <bart@vanderschans.nl>
2022-12-17 23:05:35 +0100

    Cleanup

--- 17/solve.py
+++ 17/solve.py
@@ -3,42 +3,36 @@
 import fileinput
 
 # globals
-tot = 0
-tot2 = 0
-
 W = 7
+PM = 2022
+
+# wind
 I = []
 IL = 0
 IC = 0
 
-PC = 0
-PM = 2022
-
-
-R = set()
 # floor
+R = set()
 for i in range(W):
     R.add((i, 0))
 
-RM = 0
-SC = 0
+# Pieces
+P = [
+    ((0, 0), (1, 0), (2, 0), (3, 0)),  # -
+    ((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)),  # +
+    ((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)),  # L
+    ((0, 0), (0, 1), (0, 2), (0, 3)),  # |
+    ((0, 0), (0, 1), (1, 0), (1, 1))  # 8
+]
+PL = len(P)
 
 
 for line in fileinput.input():
     l = line.strip()
     if not l:
         continue
-
-    I = list(l)
+    I = [1 if m == '>' else -1 for m in l]
     IL = len(I)
-
-# Pieces
-RT = []
-RT.append(((0, 0), (1, 0), (2, 0), (3, 0)))  # -
-RT.append(((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)))  # +
-RT.append(((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)))  # L
-RT.append(((0, 0), (0, 1), (0, 2), (0, 3)))  # |
-RT.append(((0, 0), (0, 1), (1, 0), (1, 1)))  # 8
 
 
 def dump(p=None):
@@ -55,96 +49,61 @@ def dump(p=None):
     print('     +-------+')
 
 
-def move(P, dx, dy):
-    moved = set()
-    for (x, y) in P:
-        moved.add((x+dx, y+dy))
-    return moved
+def move(p, dx, dy):
+    return set((x+dx, y+dy) for (x, y) in p)
 
 
-# helper set for finding modulo
-F = set()
+# drop the pieces
+H = []
+h = 0
+for c in range(10_000):
+    p = move(P[c % PL], 2, h+4)
+    while True:
+        # move wind
+        dx = I[IC % IL]
+        IC += 1
+        np = move(p, dx, 0)
+        if not np & R and all(0 <= x < W for (x, _) in np):
+            p = np
 
-while PC < PM:
-    t = PC % len(RT)
-    PC += 1
-    p = move(RT[t], 2, RM+4)
+        # move down
+        np = move(p, 0, -1)
+        if np & R:
+            h = max(h, max(y for (_, y) in p))
+            R |= p
+            break
 
-    # print to find cycle
-    print(PC, RM, IC, t, IC % IL, (t, IC % IL) in F)
-    F.add((t, IC % IL))
-
-    down = False  # move first then down
-    stopped = False
-    while not stopped:
-        if down:
-            down = False
-            for (x, y) in p:
-                if (x, y-1) in R:
-                    stopped = True
-                    break
-
-            if stopped:
-                for (x, y) in p:
-                    R.add((x, y))
-                    RM = max(RM, y)
-                p = set()
-                break
-
-            # move down
-            p = move(p, 0, -1)
-        else:
-            # move wind
-            down = True
-            dx = 0
-
-            d = I[IC % IL]
-            if d == '<':
-                dx = -1
-            elif d == '>':
-                dx = 1
-            else:
-                assert False, f"dir? {d}"
-
-            IC += 1
-            np = move(p, dx, 0)
-            blocked = False
-            for (x, y) in np:
-                if (x, y) in R or x < 0 or x >= W:
-                    blocked = True
-                    break
-
-            if not blocked:
-                p = np
+        p = np
+    H.append(h)
 
 
+# Find cycle
 # oberservations :
 # - tot moves is divisable by num blocks
 # - cycle based on tot blocks modulo num blocks AND  tot moves module num moves
 # - starting from R=X there is a cycle with DR=Y and DH=Z
 #
-# - Max height =  (R // DR ) * DH + val_of( R % DR + 1)
+# Formula
+# - Max height =  (R // DR ) * DH + val_of( R % DR - 1)
+dr = -1
+dh = -1
+for t in range(5, IL):
+    # assume 4 points is enough..
+    h1 = H[PM]
+    h2 = H[PM+t]
+    h3 = H[PM+t*2]
+    h4 = H[PM+t*3]
+    dh1 = h2 - h1
+    dh2 = h3 - h2
+    dh3 = h4 - h3
+    if dh1 == dh2 == dh3:
+        dh = dh1
+        dr = t
+        break
 
-print("Example")
-DR = 35
-DH = 53
-R = 2022
-P1 = R % DR + 1  # 28 rocks
-P1_VAL = 47  # 28 rock height
-print("Example1", (R//DR) * DH + P1_VAL)
-R = 1_000_000_000_000
-P1 = R % DR + 1  # 16 rocks
-P1_VAL = 25  # 16 rocks height
-print("Example2", (R//DR) * DH + P1_VAL)
+assert H[PM-1] == (PM // dr) * dh + H[PM % dr - 1]  # check formula works
 
+tot = H[PM-1]
+tot2 = (1_000_000_000_000 // dr) * dh + H[1_000_000_000_000 % dr - 1]
 
-DR = 1740
-DH = 2681
-R = 2022
-P1 = R % DR + 1  # 283 rocks
-P1_VAL = 433  # 283 rock height
-print("part1", (R//DR) * DH + P1_VAL)
-R = 1_000_000_000_000
-P1 = R % DR + 1  # 1181 rocks
-P1_VAL = 1799  # 1181 rocks height
-print("part2", (R//DR) * DH + P1_VAL)
+print(f"1: {tot}  2: {tot2}")
